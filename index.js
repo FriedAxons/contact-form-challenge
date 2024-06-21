084dcd68cd43168769b75bc5dab4f5989abd3853
FriedAxons <friedaxons@proton.me>
2024-06-21 08:52:07 -0500

    Begin adding code to handle error displays

--- index.js
+++ index.js
@@ -11,5 +11,89 @@ const textareaError = document.querySelector(".textarea-error");
 const checkbox = document.getElementById("checkbox");
 const checkboxError = document.querySelector(".consent-error");
 const submitButton = document.getElementById("submit-button");
-const successContainer = document.querySelecotr(".success-container");
+const successContainer = document.querySelector(".success-container");
 const formContainer = document.querySelector(".contact-form-container");
+
+function validateTextInput(input, errorElement) {
+  if (input.value.trim() === "") {
+    errorElement.classList.add("active");
+    return false;
+  } else {
+    errorElement.classList.remove("active");
+    return true;
+  }
+}
+
+function validateEmailInput(input, errorElement) {
+  const emailPattern = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+  if (!emailPattern.test(input.value)) {
+    errorElement.classList.add("active");
+    return false;
+  } else {
+    errorElement.classList.remove("active");
+    return true;
+  }
+}
+
+function validateRadioInput(radioInputs, errorElement) {
+  let isChecked = false;
+  radioInputs.forEach((radio) => {
+    if (radio.checked) isChecked = true;
+  });
+  if (!isChecked) {
+    errorElement.classList.add("active");
+    return false;
+  } else {
+    errorElement.classList.remove("active");
+    return true;
+  }
+}
+
+function validateCheckboxInput(checkbox, errorElement) {
+  if (!checkbox.checked) {
+    errorElement.classList.add("active");
+    return false;
+  } else {
+    errorElement.classList.remove("active");
+    return true;
+  }
+}
+
+function countActiveErrors() {
+  const errors = document.querySelectorAll(
+    ".firstname-error, .lastname-error, .email-error, .radio-error, .textarea-error, .consent-error"
+  );
+  let activeErrors = 0;
+  errors.forEach((error) => {
+    if (error.classList.contains("active")) {
+      activeErrors++;
+    }
+  });
+  return activeErrors;
+}
+
+// function adjustPadding() {}
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const isFirstNameValid = validateTextInput(firstnameInput, firstnameError);
+  const isLastNameValid = validateTextInput(lastnameInput, lastnameError);
+  const isEmailValid = validateEmailInput(emailInput, emailError);
+  const isRadioValid = validateRadioInput(radioInputs, radioError);
+  const isMessageValid = validateTextInput(messageInput, textareaError);
+  const isCheckboxValid = validateCheckboxInput(checkbox, checkboxError);
+
+  if (
+    isFirstNameValid &&
+    isLastNameValid &&
+    isEmailValid &&
+    isRadioValid &&
+    isMessageValid &&
+    isCheckboxValid
+  ) {
+    form.submit();
+  } else {
+    adjustPadding();
+  }
+});
